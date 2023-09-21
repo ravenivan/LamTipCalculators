e7e53c247e6555af55e03e7dd78168567577024d
BT_2N2_29 <sample@bths.edu>
2023-09-21 13:33:33 -0400

    Finished asking user inputs

--- TipCalculator.java
+++ TipCalculator.java
@@ -2,6 +2,10 @@ import java.util.Scanner;
 
 public class TipCalculator {
     public static void main(String[] args) {
+
+        String[] itemList;
+        int[] costs;
+
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Welcome to the tip calculator! ");
@@ -14,13 +18,24 @@ public class TipCalculator {
         System.out.println("What tip percentage would you like to give? (0-100): ");
         int tip = sc.nextInt();
 
+        String item;
         double costItem = 0;
+        double totalCost = 0;
 
         while (costItem != -1) {
             System.out.println("Enter a cost in dollars and cents, e.g. 12.50 (-1 to end): ");
             costItem = sc.nextDouble();
-
+            if (costItem != -1) {
+                totalCost += costItem;
+                System.out.println("Enter the item: ");
+                String item = sc.nextLine();
+                itemList.add(item);
+            } else {
+                break;
+            }
         }
+
+        System.out.println(totalCost);
 
 
     }
--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -2,25 +2,45 @@ import java.util.Scanner;
 
 public class TipCalculator {
     public static void main(String[] args) {
+
+        String[] itemList = new String[100];
+
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Welcome to the tip calculator! ");
         System.out.print("What is your name? ");
         String name = sc.nextLine();
 
-        System.out.println("Hello " + name + "!" + "How many people are in your group today?:  ");
+        System.out.println("Hello " + name + "!" + " How many people are in your group today?:  ");
         int people = sc.nextInt();
 
         System.out.println("What tip percentage would you like to give? (0-100): ");
         int tip = sc.nextInt();
 
+        String item;
         double costItem = 0;
+        double totalCost = 0;
 
-        while (costItem != -1) {
-            System.out.println("Enter a cost in dollars and cents, e.g. 12.50 (-1 to end): ");
+        for (int i =0 ; costItem != -1; i++) {
+            System.out.print("Enter a cost in dollars and cents, e.g. 12.50 (-1 to end): ");
             costItem = sc.nextDouble();
-
+            sc.nextLine();
+            if (costItem != -1) {
+                totalCost += costItem;
+                System.out.print("Enter the item: ");
+                item = sc.nextLine();
+                itemList[i] = item;
+            } else {
+                break;
+            }
         }
+
+        double costAfterTip = totalCost * ((tip/100.0) + 1);
+
+        System.out.println("----------------------------------");
+        System.out.println("Total bill before tip: $" + totalCost);
+        System.out.println("Total percentage: " + tip + "%");
+        System.out.println("Total bill with tip: " + (double) Math.round(costAfterTip* 100) / 100);
 
 
     }
